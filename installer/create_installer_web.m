767df82fe23241638fa517e5ac8ba13700f0f129
Venkatesh Palanivel <vp35@illinois.edu>
2025-04-23 11:30:15 -0500

    Update version to 2.2.1

--- installer/create_installer_web.m
+++ installer/create_installer_web.m
@@ -219,7 +219,7 @@ function qversion = getVersionFromGit()
 end
 
 function qversion = defaultVersion()
-    qversion = '2.1.3';
+    qversion = '2.2.1';
 end
 
 function versionString = getVersionString(tag, codeChanged)
